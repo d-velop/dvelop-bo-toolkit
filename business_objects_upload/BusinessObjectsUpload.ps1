2df2af749147e39dbb9b5ecb5732e4a82e99781e
jannikbend <94358594+jannikbend@users.noreply.github.com>
2023-01-30 12:07:35 +0100

    Updated version in upload script from 1.1.1 to 1.2.0

--- business_objects_upload/BusinessObjectsUpload.ps1
+++ business_objects_upload/BusinessObjectsUpload.ps1
@@ -19,7 +19,7 @@ function ExitHelper {
 #
 # Version of this template.
 # Version is part of the  HTTP Header-Attributes "User-Agent" and is sent with every request.
-[String] $version = "1.1.1"
+[String] $version = "1.2.0"
 
 # Script Config
 $Configuration = New-Object -TypeName ScriptConfiguration
