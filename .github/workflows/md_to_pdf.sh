d917f24030eda0ed3e96e383a49fa37c6201b758
jannikbend <94358594+jannikbend@users.noreply.github.com>
2025-03-04 19:36:41 +0100

    Update md_to_pdf.sh to replace slashes with underscores in ref names for URL generation

--- .github/workflows/md_to_pdf.sh
+++ .github/workflows/md_to_pdf.sh
@@ -8,7 +8,10 @@ echo 0 | sudo tee /proc/sys/kernel/apparmor_restrict_unprivileged_userns
 
 echo "Generating for ref name: $1"
 
-npx md-to-pdf --marked-options "{\"baseUrl\":\"https://github.com/d-velop/dvelop-bo-toolkit/tree/$1/business_objects_upload/\"}" ./business_objects_upload/README.md
-npx md-to-pdf --marked-options "{\"baseUrl\":\"https://github.com/d-velop/dvelop-bo-toolkit/tree/$1/business_objects_upload/\"}" ./business_objects_upload/CONFIGURATION.md
-npx md-to-pdf --marked-options "{\"baseUrl\":\"https://github.com/d-velop/dvelop-bo-toolkit/tree/$1/postman_collection/\"}" ./postman_collection/README.md
-npx md-to-pdf --marked-options "{\"baseUrl\":\"https://github.com/d-velop/dvelop-bo-toolkit/tree/$1/\"}" ./README.md
+# Replace / with _ in the ref name
+ref_name=$(echo $1 | sed 's/\//_/g')
+
+npx md-to-pdf --marked-options "{\"baseUrl\":\"https://github.com/d-velop/dvelop-bo-toolkit/tree/$ref_name/business_objects_upload/\"}" ./business_objects_upload/README.md
+npx md-to-pdf --marked-options "{\"baseUrl\":\"https://github.com/d-velop/dvelop-bo-toolkit/tree/$ref_name/business_objects_upload/\"}" ./business_objects_upload/CONFIGURATION.md
+npx md-to-pdf --marked-options "{\"baseUrl\":\"https://github.com/d-velop/dvelop-bo-toolkit/tree/$ref_name/postman_collection/\"}" ./postman_collection/README.md
+npx md-to-pdf --marked-options "{\"baseUrl\":\"https://github.com/d-velop/dvelop-bo-toolkit/tree/$ref_name/\"}" ./README.md
